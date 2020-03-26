e9a861d3686188fe8d7cde61ca1498346a0753db
Vandana Manhas <vandana_25@outlook.com>
2020-03-26 18:35:19 +0530

    interface changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string;
+  password: string;
+  registerUserData = {}
   constructor() { }
 
   ngOnInit() {
   }
 
+  registerUser() {
+    console.log(this.registerUserData)
+  }
 }
